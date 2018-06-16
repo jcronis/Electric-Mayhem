8770b9bf26d33838d7402d6cb5ebb00a05d7697d
ameliaHackathon <40322713+ameliaHackathon@users.noreply.github.com>
2018-06-16 16:22:59 -0400

    updated issue.js

--- issue.js
+++ issue.js
@@ -1,23 +1,31 @@
+$(function(){ 
+	function saveIssue(issue){
+	var ary = new Array();
+	ary.push({"number": issue.number});
+	ary.push({"title": issue.title});
+	ary.push({"body": issue.body});
+	ary.push({"assignee": issue.assignee});
+	var blob = new Blob([JSON.stringify(ary)], {type: "text/plain;charset=utf-8"});
+    saveAs(blob, "hello world.txt");
+   }
 
-$(function(){
- 
   function getIssue(myUrl){
   $.ajax(
-   {
-	 
+   {	 
       url: myUrl,
       type: "GET",
       headers: {'Authorization': 'token xxx'}
    }
   )
   
-  .done(function(issues){
-    console.log(issues);
+  .done(function(issue){
+		saveIssue(issue);		
+	}); 
 	
-		saveIssue();		
-	});
-    
-  }
+  } 
+
+  $("#pull").onClick(getIssue($("#url").value));
   
-    
+  
+  
 });
